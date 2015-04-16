11f5a5d6f67662ad1694e7000ccd352bb8b5a40c
Reubenscube <RKarchem@gmail.com>
2015-04-15 20:31:10 -0400

    More Exercises

--- ex14.py
+++ ex14.py
@@ -1,7 +1,7 @@
 from sys import argv
 
-script, user_name, software = argv
-prompt = '> '
+script, user_name = argv
+prompt = '-'
 
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
@@ -17,9 +17,6 @@ computer = raw_input(prompt)
 print "Which program are you using to code?"
 software = raw_input(prompt)
 
-print """
-Alright, so you said %r about liking me.
-You live in %r.  Not sure where that is.
-And you have a %r computer.
-You currently use %r to program.  Nice.
-""" % (likes, lives, computer, software)
+print "Alright, so you said %r about liking me.\nYou live in %r.  Not sure where that is.\nAnd you have a %r computer." % (likes, lives, computer)
+#instead of using the triple quotation and return, I used the \n line break.
+print "You currently use %r to program.  Nice." % software
